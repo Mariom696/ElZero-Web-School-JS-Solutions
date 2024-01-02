e9b9e6ba5baf63d88e08f35fa8d9c5a26086f802
Mario0666 <115669225+Mariom696@users.noreply.github.com>
2024-01-02 15:26:58 +0200

    Add files via upload

---
+++ Assignment 2/script.js
@@ -0,0 +1,27 @@
+//----------------------- task 1 ----------------------------
+let numOne = 10;
+let numTwo = 20;
+console.log(numOne + "" + numTwo); //1020
+console.log(typeof (numOne + "" + numTwo)); //string
+console.log(`${numOne}${numTwo}`); //1020
+console.log(typeof `${numOne}${numTwo}`);
+console.log(numOne + "\n" + numTwo);
+console.log(`${numOne}
+${numTwo}`);
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+//----------------------- task 2 ----------------------------
+console.log(
+  ' `T\'m In \n // \n Love // """ \'\'\' \n ++ With ++ \n /"""/"" \n ""JavaScript""``'
+);
+//---------------------- task 3 -----------------------------
+let a = 21;
+let b = 20; // _21_2021_2021_2021_20_
+console.log(
+  a + "_" + b + "" + a + "_" + b + "" + a + "_" + b + "" + a + "_" + b + "_"
+);
+/*
+-------------------------------------------
+there is alot of soultions so be flexible |
+-------------------------------------------
+*/
