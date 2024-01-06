fe6f4704245afa62e4ed848a5c70af415b7b6884
Mario0666 <115669225+Mariom696@users.noreply.github.com>
2024-01-06 19:11:25 +0200

    Add files via upload

---
+++ Assignment 5/script.js
@@ -0,0 +1,34 @@
+//----------------------- string challnges -------------------
+let a = "Elzero Web School";
+
+console.log(a.charAt(2).toUpperCase() + a.slice(3, 6));
+
+console.log(a.charAt(13).repeat(8).toUpperCase());
+
+console.log(a.split("", 6));
+
+console.log(`${a.substr(0, 6)} ${a.substr(11, 6)}`);
+
+console.log(
+  a.charAt(0).toLowerCase() +
+    a.substring(1, a.length - 1).toUpperCase() +
+    a.charAt(a.length - 1).toLowerCase()
+);
+//---------------------- task 1 -------------------------------
+let userName = "Elzero";
+console.log(userName.slice(3, 4)); // e
+console.log(userName[3]); // e
+console.log(userName.charAt(3)); // e
+console.log(userName.substring(3, 4)); // e
+console.log(userName.substr(3, 1)); // e
+console.log(userName[3].repeat(3)); // eee
+//---------------------- task 2 --------------------------------
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.substring(3).startsWith(letterE)); // True
+console.log(word.toUpperCase().endsWith(letterO)); // True
+//---------------------------------------------------------------
