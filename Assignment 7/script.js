cd73e6a1ebac39fa10d4b6466f8aa5eb65f38976
Mario0666 <115669225+Mariom696@users.noreply.github.com>
2024-01-18 00:09:41 +0200

    Update script.js

--- Assignment 7/script.js
+++ Assignment 7/script.js
@@ -24,3 +24,12 @@ let st = "Elzero Web School";
 if ((st.length + st.length).toString() === "34") {
   console.log("good");
 }
+//-------------------------- task 1 --------------------
+let num = 9;
+if (num < 10) {
+  console.log(`00${num}`);
+} else if (num > 10 && num < 100) {
+  console.log(`0${num}`);
+} else {
+  console.log(num);
+}
