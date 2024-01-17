547f42291c5e3130a27cbb90b9fd4510e7d057f7
Mario0666 <115669225+Mariom696@users.noreply.github.com>
2024-01-18 00:10:33 +0200

    Update script.js

--- Assignment 7/script.js
+++ Assignment 7/script.js
@@ -33,3 +33,26 @@ if (num < 10) {
 } else {
   console.log(num);
 }
+//---------------------------task 2 -------------------
+
+let num1 = 9;
+let str = "9";
+let str2 = "20";
+if (num1 == str) {
+  console.log(`"{${num1}} is the same value as {${str}}"`);
+  if (num1 !== str) {
+    console.log(
+      `"{${num1}} is the same value as {${str}} but not the same type"`
+    );
+  }
+}
+if (num1 !== str2) {
+  console.log(
+    `"{${num1}} is  not the same value or the same type as {${str2}} "`
+  );
+}
+if (str !== str2) {
+  console.log(
+    `"{${str}} is the same type as {${str2}} but not the same value"`
+  );
+}
