f2945c1928d13857294ba7e85b21e34ab7658869
Mario0666 <115669225+Mariom696@users.noreply.github.com>
2024-01-26 23:25:30 +0200

    Update script.js

--- Assignment 8/script.js
+++ Assignment 8/script.js
@@ -3,4 +3,5 @@ console.log("hello world);
 console.log("tomoroow i will upload the rest of the files ")
 window.write("happy coding ")
 console.log(" hey i see you ")
+//------- this is a comment --------------------------------
             
