0dd102b9565d37edae89d8f6ea31a754bd1dafdb
Mario0666 <115669225+Mariom696@users.noreply.github.com>
2024-01-30 13:41:11 +0200

    Delete Assignment 8 directory

--- Assignment 8/script.js
+++
@@ -1,7 +0,0 @@
-console.log(10 * "_");
-console.log("hello world);
-console.log("tomoroow i will upload the rest of the files ")
-window.write("happy coding ")
-console.log(" hey i see you ")
-//------- this is a comment --------------------------------
-            
